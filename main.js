5ed98db8cf2462b35f3712814aa893d7a04bc82c
Harold Roberson <hdroberson@gmail.com>
2018-12-26 21:29:19 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+class book {
+    constructor(title, author){
+    this.title = title;
+    this.author = author;
+    }
+}
+
+let bookOne = new book ('Star Wars ', 'George Lucas');
+let bookTwo = new book ('Ulysses ', 'James Joyce');
+let bookThree = new book ('Moby Dick ', 'Herman Melville');
+let bookFour = new book ('Hamlet ', 'William Shakespeare');
+
+console.log('bookOne');
+console.log('bookTwo');
+console.log('bookThree');
+console.log('bookFour');
